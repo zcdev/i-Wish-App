b02e87b04e50fa52638abb174649f7253a005579
ZCDEV <zoechangdev@gmail.com>
2019-09-21 23:40:19 -0700

    Formatted Date with vue-moment

--- src/components/Wishes.vue
+++ src/components/Wishes.vue
@@ -3,7 +3,7 @@
     <ul class="posts">
       <li class="wish" v-for="(wish, idx) in wishes" :key="idx">
           <p class="message">{{ wish.message }}</p>
-          <p class="info"><span class="name">{{ wish.name }}</span><br>{{ wish.createdAt.toDate() }}</p>
+          <p class="info"><span class="name">{{ wish.name }}</span><br>{{ wish.createdAt.toDate() | moment("dddd, MMMM Do YYYY, h:mm:ss A") }}</p>
           <button v-if="wish.userId == user" class="button" @click="deleteWish(wish.id)">
             My wish came true
           </button>
@@ -32,7 +32,6 @@ export default {
       name: firebase.auth().currentUser.displayName,
       userId: '',
       user: firebase.auth().currentUser.uid,
-      createdAt: new Date()
     }
   },
   firestore() {
@@ -42,6 +41,7 @@ export default {
   },
   methods: {
     addWish(message) {
+      const createdAt = new Date();
       const userId = firebase.auth().currentUser.uid;
       const name = firebase.auth().currentUser.displayName;
       db.collection('wishes').add({ message, name, userId, createdAt });
@@ -81,11 +81,117 @@ min-height: 100vh;
 padding: 1rem;
 }
 .message {font-size: 1.5em;}
-.info {font-size: 1em; position: absolute; bottom: 0; font-family: Verdana, sans-serif;}
+.info {font-size: 0.7em; position: absolute; bottom: 0; font-family: Verdana, sans-serif;}
 .name {
-  font-family: 'Satisfy', cursive; font-size: 1em;
+  font-family: 'Satisfy', cursive; font-size: 1.5em;
 }
 textarea { font-size: 1em; min-width: 19rem; min-height: 20em; background: rgba(255, 255, 255, 0.7); color: #000;}
+
+ul,
+li {
+  list-style: none;
+}
+
+ul {
+  overflow: hidden;
+  margin: 0;
+}
+
+ul li {
+  text-decoration: none;
+  color: #000;
+  background: #ffc0c8;
+  display: block;
+  height: 20em;
+  width: 20em;
+  padding: 1em;
+  -moz-box-shadow: 5px 5px 7px rgba(33, 33, 33, 1);
+  -webkit-box-shadow: 5px 5px 7px rgba(33, 33, 33, .7);
+  box-shadow: 5px 5px 7px rgba(33, 33, 33, .7);
+}
+
+ul li {
+  margin: 1em;
+  float: left;
+}
+
+ul li h2 {
+  font-size: 140%;
+  font-weight: bold;
+  padding-bottom: 10px;
+}
+
+ul li p {
+  font-family: 'Satisfy', cursive;
+  font-size: 180%;
+}
+
+ul li {
+  -webkit-transform: rotate(-6deg);
+  -o-transform: rotate(-6deg);
+  -moz-transform: rotate(-6deg);
+}
+
+ul li:nth-child(even) {
+  -o-transform: rotate(4deg);
+  -webkit-transform: rotate(4deg);
+  -moz-transform: rotate(4deg);
+  position: relative;
+  top: 5px;
+  background: #ffdfb6;
+}
+
+ul li:nth-child(3n) {
+  -o-transform: rotate(-3deg);
+  -webkit-transform: rotate(-3deg);
+  -moz-transform: rotate(-3deg);
+  position: relative;
+  top: -5px;
+  background: #d3b6e5;
+}
+
+ul li:nth-child(5n) {
+  -o-transform: rotate(5deg);
+  -webkit-transform: rotate(5deg);
+  -moz-transform: rotate(5deg);
+  position: relative;
+  top: -10px;
+}
+
+ul li:hover,
+ul li a:focus {
+  box-shadow: 10px 10px 7px rgba(0, 0, 0, .7);
+  -moz-box-shadow: 10px 10px 7px rgba(0, 0, 0, .7);
+  -webkit-box-shadow: 10px 10px 7px rgba(0, 0, 0, .7);
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
+  position: relative;
+  z-index: 5;
+}
+
+.makewish {
+  width: 24%;
+  float: right;
+  position: fixed;
+  top: 0;
+  right: 0;
+  padding-left: 2rem;
+}
+
+.posts {
+  width: 76%;
+
+}
+
+@media screen and (max-width: 500px) {
+  .posts,
+  .makewish {
+    width: 100%;
+    display: block;
+    position: relative;
+  }
+}
 
 
 </style>
